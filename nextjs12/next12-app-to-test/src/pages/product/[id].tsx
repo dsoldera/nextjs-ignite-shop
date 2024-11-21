5324f7f39077a5e3709bfcd7623f5cb00d04dea1
dsoldera <daniellesoldera@gmail.com>
2024-11-21 16:26:54 -0300

    update products page and checkout api

--- nextjs12/next12-app-to-test/src/pages/product/[id].tsx
+++ nextjs12/next12-app-to-test/src/pages/product/[id].tsx
@@ -1,15 +1,99 @@
+import axios from "axios";
+import { GetStaticPaths, GetStaticProps } from "next";
+import Image from "next/image";
+import { useRouter } from "next/router";
+import { useState } from "react";
+import Stripe from "stripe";
+import { stripe } from "../../lib/stripe";
+import { ImageContainer, ProductContainer, ProductDetails } from "../../styles/pages/product";
 
-export default function Product() {
+interface ProductProps {
+  product: {
+    id: string
+    name: string
+    imageUrl: string
+    price: string
+    description: string
+    defaultPriceId: string
+  }
+}
+
+export default function Product({ product }: ProductProps) {
+  const [isCreatingCheckoutSession, setIsCreatingCheckoutSession] = useState(false);
+  const { isFallback } = useRouter()
+
+  if (isFallback) return <p>Loading...</p>
+  
+  const handleBuyButton = async () => {
+    try{
+      const response = await axios.post('/api/checkout', {
+        priceId: product.defaultPriceId,
+      })
+
+      const { checkoutUrl } = response.data;
+
+      window.location.href = checkoutUrl;
+
+    } catch(err) {
+      alert('falha no carregamento do Checkout')
+    }
+    console.log(product.defaultPriceId);
+  }
+
   return(
-    <h1>Hello</h1>
+    <ProductContainer>
+      <ImageContainer>
+        <Image src={product.imageUrl} width={400} height={400} alt="" />
+      </ImageContainer>
+      <ProductDetails>
+        <h1>{product.name}</h1>
+        <span>{product.price}</span>
+        <p>{product.description}</p>
+        <button disabled={isCreatingCheckoutSession} onClick={handleBuyButton}>
+          Comprar agora
+        </button>
+      </ProductDetails>
+    </ProductContainer>
   )
 }
 
-// export const getStaticProps: GetStaticProps<any, { id: string }> = async () => {
+// A method that returns the Ids that I can generate the static page
+// So I can pass the most visited products
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [
+      {
+        params: { id: 'prod_REbIEmSA8rSe8t' }
+      }
+    ],
+    fallback: true,
+    // fallback: "blocking",
+  }
+}
 
-//   return(
-//     props: {
-//       product: {}     
-//     }
-//   )
-// }
+export const getStaticProps: GetStaticProps<any, { id: string }> = async ({ params }) => {
+  const productId = params!.id
+
+  const product = await stripe.products.retrieve(productId, {
+    expand: ['default_price']
+  })
+
+  const price = product.default_price as Stripe.Price;
+
+  return {
+    props: {
+      product: {
+        id: product.id,
+        name: product.name,
+        imageUrl: product.images[0],
+        price: new Intl.NumberFormat('pt-BR', {
+          style: 'currency',
+          currency: 'BRL'
+        }).format(price.unit_amount! / 100),
+        description: product.description,
+        defaultPriceId: price.id
+      }
+    },
+    revalidate: 60 * 60 * 1 // 1 hour
+  }
+}
